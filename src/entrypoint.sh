3a58de1c017bbdd8a73fc68c9f793d75ded17c00
barrettMCW <103449618+barrettMCW@users.noreply.github.com>
2024-02-06 10:27:08 -0600

    Create entrypoint.sh

---
+++ src/entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Check environment variable to determine the mode
+case "$MODE" in
+  OPERATOR)
+    echo "Running as Operator..."
+    python3 /app/OmeroDropboxOperator.py
+    ;;
+  WEBHOOK)
+    echo "Running as Webhook..."
+    gunicorn --workers=${GUNICORN_WORKERS} --bind=0.0.0.0:8000 /app/OmeroImportWebhook:app
+    ;;
+  WATCH)
+    echo "Running as Watch..."
+    # Ensure that WATCHED_DIR, WEBHOOK_URL, and IGNORE_PATTERN are set
+    if [ -z "$WATCHED_DIR" ] || [ -z "$WEBHOOK_URL" ] || [ -z "$WATCH_NAME" ]; then
+        echo "The WATCHED_DIR and WEBHOOK_URL environment variables must be set."
+        exit 1
+    fi
+    
+    # Optional: Regex pattern to ignore files
+    IGNORE_PATTERN="${IGNORE_PATTERN:-}"
+    
+    should_ignore_file() {
+        local file_path=$1
+        if [[ $file_path =~ $IGNORE_PATTERN ]]; then
+            return 0 # True, should ignore
+        fi
+        return 1 # False, should not ignore
+    }
+    
+    # Function to send file path to the webhook
+    send_to_webhook() {
+        local file_path=$1
+        if should_ignore_file "$file_path"; then
+            echo "Ignoring file: $file_path"
+            return
+        fi
+        curl -X POST "$WEBHOOK_URL" -H "Content-Type: application/json" -d "{\"OmeroDropbox\":\"$WATCH_NAME\", \"fullPath\":\"$file_path\"}"
+    }
+    
+    # Loop through existing files and send them to the webhook
+    find "$WATCHED_DIR" -type f -print0 | while IFS= read -r -d $'\0' file; do
+        send_to_webhook "$file"
+    done
+    
+    # Start inotifywait to monitor new files and send them to the webhook
+    inotifywait -m -e close_write --format '%w%f' "$WATCHED_DIR" | while read file; do
+        send_to_webhook "$file"
+    done
+    ;;
+  *)
+    echo "No valid MODE specified. Please set MODE to OPERATOR, WEBHOOK, or WATCH."
+    exit 1
+    ;;
+esac
